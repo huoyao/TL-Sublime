8ad49b701709bd941fd1cf5472a7d56ea4079974
zjutangle@mail.com <huoyaotl123@126.com>
2015-04-09 18:57:11 +0800

    sublime push  input.cpp --add

---
+++ input.cpp
@@ -0,0 +1,17 @@
+/*input
+122
+*/
+#include <iostream>
+#include <vector>
+#include <cstdio>
+using namespace std;          
+
+int main()
+{
+	freopen("input.txt","r",stdin);
+	int t;
+	cin>>t;
+	cout<<"wow 哈哈:"<<t<<endl;
+	cout<<"text good "<<endl;
+	return 0;
+}

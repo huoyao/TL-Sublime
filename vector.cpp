c11b2b089f2ca0000dc5acceda4a341fadb6ca97
huoyao <huoyaotl123@126.com>
2015-07-25 16:14:01 +0800

    vector test --add

--- vector.cpp
+++ vector.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+//#include "outputInfo.h"
 using namespace std;
 
 void test_vector_pop_back()
@@ -8,14 +9,33 @@ void test_vector_pop_back()
 	vec.push_back(0);
 	vec.pop_back();
 	vec.pop_back();
-	cout<<"succeed"<<endl;
+	vec[0]=1;
+	cout<<"succeed"<<"  vec[0]="<<vec[0]<<endl;
+}
+
+template <class T>
+void print_info(const T &vec)
+{
+	std::cout<<"    size:"<<vec.size()<<std::endl;
+	std::cout<<"capacity:"<<vec.capacity()<<std::endl;
+}
+
+void test_vector_size_capacity()
+{
+	std::vector<int> vec;
+	print_info(vec);
+	vec.push_back(1);
+	print_info(vec);
+	vec.push_back(2);
+	print_info(vec);
+	vec.push_back(3);
+	print_info(vec);
 }
 
 int main()
 {
-	int t;
-	cin>>t;
-	test_vector_pop_back();
+	//test_vector_pop_back();
+	test_vector_size_capacity();
 	getchar();
 	return 0;
 }
